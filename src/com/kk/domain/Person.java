724cde44050553518c92149c12c132ae8bf5526e
zsy <zsy131859666961@163.com>
2025-04-20 11:46:45 +0800

    zsy3提交了get set方法

--- src/com/kk/domain/Person.java
+++ src/com/kk/domain/Person.java
@@ -10,4 +10,16 @@ public class Person {
     private String name;
     private int age;
     private double score;
+
+    public double getScore() {
+        return score;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
